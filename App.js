64ecb454f5f80dc2f4a437f7a3c0a9bc439a90fc
lopesjr <atlopesjunior@gmail.com>
2021-04-04 20:51:04 -0300

    routes creation

--- App.js
+++ App.js
@@ -1,53 +1,14 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Appbar } from 'react-native-paper';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './HomeScreen';
-import DetailsScreen from './DetailsScreen';
-
-const Stack = createStackNavigator();
-
-function CustomNavigationBar({ navigation, previous}) {
-  return (
-    <Appbar.Header>
-      {previous ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
-      <Appbar.Content title="Frete app" />
-    </Appbar.Header>
-  )
-}
+import Main from './src/index';
 
 class App extends React.Component {
   render() {
     return (
-      <NavigationContainer>
-        <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions = {{
-          header: (props) => <CustomNavigationBar {...props} />,
-        }}>
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-          />
-          <Stack.Screen
-            name="Details"
-            component={DetailsScreen}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Main>
+      </Main>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
---
+++ src/index.js
@@ -0,0 +1,53 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Appbar } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/pages/HomeScreen';
+import DetailsScreen from './src/pages/DetailsScreen';
+
+const Stack = createStackNavigator();
+
+function CustomNavigationBar({ navigation, previous}) {
+  return (
+    <Appbar.Header>
+      {previous ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
+      <Appbar.Content title="Frete app" />
+    </Appbar.Header>
+  )
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions = {{
+          header: (props) => <CustomNavigationBar {...props} />,
+        }}>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+          />
+          <Stack.Screen
+            name="Details"
+            component={DetailsScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
