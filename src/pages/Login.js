fca11c6c89f3b85911b62a4867a9d14314bff87a
lopesjr <atlopesjunior@gmail.com>
2021-06-04 15:40:19 -0300

    login verify

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,42 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import {StyleSheet, Text, View, Button, Alert} from 'react-native';
 import {UseUserData} from '../context/userContext';
-import {TextInput} from 'react-native-paper';
+import {TextInput, Snackbar} from 'react-native-paper';
 import auth from '@react-native-firebase/auth';
 
 const Login = ({navigation}) => {
   const {setUserType} = UseUserData();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isValid, setValid] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const onToggleSnackBar = () => setVisible(!visible);
+  const onDismissSnackBar = () => setVisible(false);
+
+  const verifySignIn = () => {
+    if (!email) {
+      setError('Email required *');
+      setValid(false);
+      return;
+    } else if (!password && !password.trim()) {
+      setError('Password required *');
+      setValid(false);
+      return;
+    }
+    doSignIn(email, password);
+  };
+
+  const doSignIn = async (email, password) => {
+    try {
+      let response = await auth().signInWithEmailAndPassword(email, password);
+      if (response && response.user) {
+        Alert.alert('Success', 'Authenticated successfully.');
+      }
+    } catch (e) {
+      onToggleSnackBar();
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -20,29 +49,33 @@ const Login = ({navigation}) => {
           style={{width: '100%', height: 60}}
           label="Email"
           value={email}
-          onChangeText={text => setEmail(text)}
+          onChangeText={text => {
+            setError;
+            setEmail(text);
+          }}
+          error={isValid}
         />
       </View>
 
       <View style={styles.inputStyle}>
         <TextInput
           style={{width: '100%', height: 60}}
+          secureTextEntry
           label="Password"
           value={password}
+          error={isValid}
           onChangeText={text => setPassword(text)}
         />
       </View>
 
+      {error ? (
+        <View style={styles.errorLabelContainerStyle}>
+          <Text style={styles.errorTextStyle}>{error}</Text>
+        </View>
+      ) : null}
+
       <View style={styles.btnStyle}>
-        <Button
-          title="LOGIN"
-          onPress={() => {
-            auth()
-              .signInWithEmailAndPassword(email, password)
-              .then(() => {})
-              .catch(() => console.log('erro ao logar'));
-          }}
-        />
+        <Button title="LOGIN" onPress={verifySignIn} />
       </View>
 
       <View style={styles.btnStyle}>
@@ -51,6 +84,18 @@ const Login = ({navigation}) => {
           onPress={() => navigation.navigate('RegisterUser')}
         />
       </View>
+
+      <Snackbar
+        visible={visible}
+        onDismiss={onDismissSnackBar}
+        action={{
+          label: 'Close',
+          onPress: () => {
+            // Do something
+          },
+        }}>
+        User not found! Try again.
+      </Snackbar>
 
       {/* <View style={styles.btnStyle}>
         <Button
@@ -82,6 +127,15 @@ const styles = StyleSheet.create({
   },
   logoStyle: {
     paddingBottom: 10,
+  },
+  errorLabelContainerStyle: {
+    flex: 0.1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  errorTextStyle: {
+    color: 'red',
+    textAlign: 'center',
   },
 });
 
